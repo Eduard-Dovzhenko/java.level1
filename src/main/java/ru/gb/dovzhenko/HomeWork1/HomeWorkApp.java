5e416c513f5a741d0ce504c5ad6e711fc7955fa9
Eduard-Dovzhenko <dovj26rus@outlook.com>
2022-04-12 21:24:34 +0500

    second task

--- src/main/java/ru/gb/dovzhenko/HomeWork1/HomeWorkApp.java
+++ src/main/java/ru/gb/dovzhenko/HomeWork1/HomeWorkApp.java
@@ -2,6 +2,9 @@ package ru.gb.dovzhenko.HomeWork1;
 
 public class HomeWorkApp {
     public static void main(String[] args) {
-
+        printThreeWords();
+    }
+    public static void printThreeWords(){
+        System.out.println("Orange\nBanana\nApple");
     }
 }
