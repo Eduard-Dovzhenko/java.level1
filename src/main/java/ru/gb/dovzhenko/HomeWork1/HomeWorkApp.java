3793003162991696aab780102de52bd40cf10d36
Eduard-Dovzhenko <dovj26rus@outlook.com>
2022-04-12 21:35:39 +0500

    third task

--- src/main/java/ru/gb/dovzhenko/HomeWork1/HomeWorkApp.java
+++ src/main/java/ru/gb/dovzhenko/HomeWork1/HomeWorkApp.java
@@ -3,8 +3,18 @@ package ru.gb.dovzhenko.HomeWork1;
 public class HomeWorkApp {
     public static void main(String[] args) {
         printThreeWords();
+        checkSumSign();
     }
     public static void printThreeWords(){
         System.out.println("Orange\nBanana\nApple");
+    }
+    public static void checkSumSign(){
+        int a = 5 , b = 6 ;
+        if ( a + b >= 0 ){
+            System.out.println("Сумма положительная");
+        }
+        else {
+            System.out.println("Сумма отрицательная");
+        }
     }
 }
