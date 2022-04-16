59c0357948de3494cb7c0883e82e1f680f4f82e4
Eduard-Dovzhenko <dovj26rus@outlook.com>
2022-04-16 12:30:14 +0500

    task one

--- src/main/java/ru/gb/dovzhenko/HomeWork2.java
+++ src/main/java/ru/gb/dovzhenko/HomeWork2.java
@@ -2,7 +2,15 @@ package ru.gb.dovzhenko;
 
 public class HomeWork2 {
     public static void main(String[] args) {
-
+        summLimits();
     }
 
+    public static void summLimits() {
+        int a = 5, b = 7;
+        if (a + b >= 10 && a + b <= 20) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+    }
 }
