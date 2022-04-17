5714837d519f6aa48924c063233c3f4361f0e1aa
Eduard-Dovzhenko <dovj26rus@outlook.com>
2022-04-17 15:10:55 +0500

    Corrected tasks 2,3,4

--- src/main/java/ru/gb/dovzhenko/HomeWork2.java
+++ src/main/java/ru/gb/dovzhenko/HomeWork2.java
@@ -4,10 +4,17 @@ public class HomeWork2 {
     public static void main(String[] args) {
         int a = 5, b = 7;
         summLimits2(a, b);
-        positiveOrNegativeNumber();
+
+        int f = 5;
+        positiveOrNegativeNumber(f);
+
         int d = 22;
         positiveOrNegativeNumber2(d);
-        linePrinting();
+
+        String g = "acer";
+        int h = 5;
+        linePrinting(g, h);
+
         int e = 1900;
         leapYear(e);
     }
@@ -16,27 +23,25 @@ public class HomeWork2 {
         return (a + b >= 10 && a + b <= 20);
     }
 
-    public static void positiveOrNegativeNumber() {
-        int a = 0;
-        if (a % 2 == 0) {
-            System.out.println("Число чётное");
+    public static void positiveOrNegativeNumber(int f) {
+        if (f >= 0) {
+            System.out.println("Число положительное");
         } else {
-            System.out.println("Число нечётное");
+            System.out.println("Число отрицательное");
         }
     }
 
     public static boolean positiveOrNegativeNumber2(int d) {
-        if (d % 2 != 0) {
-            return true;
-        } else {
+        if (d >= 0) {
             return false;
+        } else {
+            return true;
         }
     }
 
-    public static void linePrinting() {
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Число 15");
-
+    public static void linePrinting(String g, int h) {
+        for (int i = 0; i < h; i++) {
+            System.out.println(g);
         }
     }
 
