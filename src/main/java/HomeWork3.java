c9b2afbbc2ff5ec09eb3f70c9112edc28897957d
Eduard-Dovzhenko <dovj26rus@outlook.com>
2022-04-22 20:12:51 +0500

    task one

--- src/main/java/HomeWork3.java
+++ src/main/java/HomeWork3.java
@@ -1,5 +1,20 @@
+import java.util.Arrays;
+
 public class HomeWork3 {
     public static void main(String[] args) {
+        invertArray();
+    }
 
+    public static void invertArray() {
+        int[] arr = {1, 1, 0, 0, 1, 0, 1, 0};
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) {
+                arr[i] = 1;
+            } else {
+                arr[i] = 0;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
     }
 }
+
