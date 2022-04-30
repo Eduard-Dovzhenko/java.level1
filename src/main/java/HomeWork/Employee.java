1c9098ce120195b2ffd20f6ccbf9804cbd76b1ef
Eduard-Dovzhenko <dovj26rus@outlook.com>
2022-04-30 09:10:18 +0500

    HomeWork5 (1)

--- src/main/java/HomeWork/Employee.java
+++ src/main/java/HomeWork/Employee.java
@@ -1,12 +1,62 @@
 package HomeWork;
 
 public class Employee {
-    public String fullName;
-    public String position;
-    public String email;
-    public int phoneNumber;
-    public double theSalary;
-    public int age;
+    private String fullName;
+    private String position;
+    private String email;
+    private int phoneNumber;
+    private double theSalary;
+    private int age;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public double getTheSalary() {
+        return theSalary;
+    }
+
+    public void setTheSalary(double theSalary) {
+        this.theSalary = theSalary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age >= 0 && age < 150) {
+            this.age = age;
+        }
+    }
 
     public Employee(String fullName, String position, String email, int phoneNumber, double theSalary, int age) {
         this.fullName = fullName;
@@ -14,7 +64,7 @@ public class Employee {
         this.email = email;
         this.phoneNumber = phoneNumber;
         this.theSalary = theSalary;
-        this.age = age;
+        setAge(age);
     }
 
     public static void main(String[] args) {
