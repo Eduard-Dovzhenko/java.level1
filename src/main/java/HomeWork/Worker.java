b03e83c96434570befaa709a7730d7bc29c7ef24
Eduard-Dovzhenko <dovj26rus@outlook.com>
2022-04-30 09:14:10 +0500

    HomeWork5 (2)

---
+++ src/main/java/HomeWork/Worker.java
@@ -0,0 +1,35 @@
+package HomeWork;
+
+public class Worker {
+    public static void main(String[] args) {
+        Employee worker = new Employee("Иванов Иван Иванович", "Начальник отдела", "a@a.com", 891255545,
+                70056.50, 56);
+        String info = "ФИО: " + worker.getFullName() + "\nДолжность: " + worker.getPosition() + "\nПочта: " + worker.getEmail() + "\nНомер телефона: "
+                + worker.getPhoneNumber() + "\nЗарплата: " + worker.getTheSalary() + "\nВозраст: " + worker.getAge();
+        System.out.println(info);
+        System.out.println();
+
+        Employee[] workerArray = new Employee[5];
+        workerArray[0] = new Employee("Петров Петр Петрович", "Начальник ", "b@b.com", 891255534,
+                60056.50, 46);
+        workerArray[1] = new Employee("Комаров Виктор Анатольевич", "Слесарь", "с@c.com", 891245545,
+                32456.80, 58);
+        workerArray[2] = new Employee("Смирнов Борис Инакентиевич", "Наладчик", "d@d.com", 890658754,
+                36666.30, 32);
+        workerArray[3] = new Employee("Жабин Константин Романович", "Слесарь КИПиА", "e@e.com", 890545878,
+                33000.47, 36);
+        workerArray[4] = new Employee("Андронов Арсений Александрович", "Инженер", "f@f.com", 892835366,
+                41550.63, 38);
+        for (int i = 0; i < workerArray.length; i++) {
+            if (workerArray[i].getAge() > 40) {
+                String info1 = "ФИО: " + workerArray[i].getFullName() + "\nДолжность: " + workerArray[i].getPosition() + "\nПочта: "
+                        + workerArray[i].getEmail() + "\nНомер телефона: " + workerArray[i].getPhoneNumber() + "\nЗарплата: " +
+                        workerArray[i].getTheSalary() + "\nВозраст: " + workerArray[i].getAge();
+                System.out.println(info1);
+                System.out.println();
+            }
+
+
+        }
+    }
+}
