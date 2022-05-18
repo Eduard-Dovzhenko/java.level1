25ce74190869a7b8479ce3166b704595ea1e6796
Eduard-Dovzhenko <dovj26rus@outlook.com>
2022-05-18 09:29:23 +0500

    HomeWork7 2

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,9 +1,15 @@
 public class MainClass {
     public static void main(String[] args) {
-        Cat cat = new Cat("Barsik", 5);
+        Cat[] cat = new Cat[5];
+        cat[0] = new Cat("Barsik", 15);
+        cat[1] = new Cat("Vantuz", 12);
+        cat[2] = new Cat("Kotzila", 10);
+        cat[3] = new Cat("Timofei", 17);
+        cat[4] = new Cat("Murzik", 19);
         Plate plate = new Plate(100);
         plate.info();
-        cat.eat(plate);
-        plate.info();
+        for (int i = 0; i < cat.length; i++) {
+            cat[i].eat(plate);
+        }plate.info();
     }
 }
